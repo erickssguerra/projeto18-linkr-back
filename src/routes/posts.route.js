dcd1ca7e0175cd514ad4029ed394d56ecb155173
JoaoVRP <joaovrp.mec.ufsc@gmail.com>
2023-01-04 22:23:33 -0300

    refact: changed post schema and post route middlewares

--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import { getPosts, publishPost } from "../controllers/posts.controllers.js";
+import { authValidation } from "../middlewares/authValidation.middleware.js";
 import { validateBody } from "../middlewares/schema.middleware.js";
 import { postSchema } from "../schemas/post.schema.js";
 
 const router = Router();
 
 router.get("/timeline", getPosts);
-router.post("/timeline", validateBody(postSchema, "timeline"), publishPost);
+router.post("/timeline", authValidation, validateBody(postSchema, "timeline"), publishPost);
 
 export default router;
