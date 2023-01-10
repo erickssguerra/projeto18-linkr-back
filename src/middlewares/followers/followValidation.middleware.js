8a1c22e55ac1a81ebd7a1a4d6022432bc7cbff43
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-01-10 15:55:13 -0300

    feat: added error messages

--- src/middlewares/followers/followValidation.middleware.js
+++ src/middlewares/followers/followValidation.middleware.js
@@ -1,22 +1,32 @@
 import followersRepositories from "../../repositories/followers.repositories.js";
+import usersRepositories from "../../repositories/users.repositories.js";
 
 export async function followValidation(req, res, next) {
   const { userId } = req.user;
   const { id } = res.locals.followerId;
   const followerId = parseInt(id);
 
-  const followers = await followersRepositories.getFollowersById(
-    userId,
-    followerId
-  );
-
-  if (followers[0].count !== "0") {
-    return res
-      .status(500)
-      .send({ message: "You are already following this user!" });
-  }
-
   try {
+    if (userId === followerId) {
+      return res.status(500).send({ message: "You can't follow yourself" });
+    }
+
+    const followerExists = await usersRepositories.selectUserById(followerId);
+
+    if (followerExists.length === 0) {
+      return res.status(404).send({ message: "User not found!" });
+    }
+
+    const followers = await followersRepositories.getFollowersById(
+      userId,
+      followerId
+    );
+
+    if (followers[0].count !== "0") {
+      return res
+        .status(500)
+        .send({ message: "You are already following this user!" });
+    }
   } catch (err) {
     console.log(err);
     return res.sendStatus(500);
--- src/middlewares/followers/unfollowValidation.middleware.js
+++ src/middlewares/followers/unfollowValidation.middleware.js
@@ -1,22 +1,34 @@
 import followersRepositories from "../../repositories/followers.repositories.js";
+import usersRepositories from "../../repositories/users.repositories.js";
 
 export async function unfollowValidation(req, res, next) {
   const { userId } = req.user;
   const { id } = res.locals.followerId;
   const followerId = parseInt(id);
 
-  const followers = await followersRepositories.getFollowersById(
-    userId,
-    followerId
-  );
-
-  if (followers[0].count === "0") {
-    return res
-      .status(500)
-      .send({ message: "You don't follow this user!" });
-  }
-
   try {
+    if (userId === followerId) {
+      return res.status(500).send({ message: "You can't unfollow yourself" });
+    }
+
+    const followerExists = await usersRepositories.selectUserById(followerId);
+
+    if (followerExists.length === 0) {
+      return res.status(404).send({ message: "User not found!" });
+    }
+
+    const followers = await followersRepositories.getFollowersById(
+      userId,
+      followerId
+    );
+  
+    if (followers[0].count === "0") {
+      return res
+        .status(500)
+        .send({ message: "You don't follow this user!" });
+    }
+
+
   } catch (err) {
     console.log(err);
     return res.sendStatus(500);
