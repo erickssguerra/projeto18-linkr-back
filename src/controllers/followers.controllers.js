500cc6a904b28581818969e309acaef51affe080
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-01-10 23:30:54 -0300

    refactor: deleted message when user doesn't follow anyone

--- src/controllers/followers.controllers.js
+++ src/controllers/followers.controllers.js
@@ -6,12 +6,6 @@ export async function getFollowers(req, res) {
   try {
     const followers = await followersRepositories.getFollowers(userId);
 
-    if (followers.length === 0) {
-      return res.status(200).send({
-        message: "You don't follow anyone yet. Search for new friends!",
-      });
-    }
-
     res.status(200).send(followers);
   } catch (err) {
     console.log(err);
