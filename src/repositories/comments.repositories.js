1a0ed548fd4ef5df9913edfccc9de0bf22d52fbc
JoaoVRP <joaovrp.mec.ufsc@gmail.com>
2023-01-12 16:32:47 -0300

    feat: getComments function

--- src/repositories/comments.repositories.js
+++ src/repositories/comments.repositories.js
@@ -12,6 +12,38 @@ async function postComment(user_id, post_id, comment) {
   );
 }
 
+async function getComments(post_id) {
+  const {rows} = await connectionDB.query(
+    `
+    SELECT
+      c.user_id AS comment_author_id, c.comment,
+      u.name, u.picture_url,
+      ARRAY(
+        SELECT
+          f.user_id
+        FROM
+          followers f
+        WHERE
+          f.followed_id = c.user_id
+      ) AS followers
+    FROM
+      comments c
+    JOIN
+      users u
+    ON
+      c.user_id = u.id
+    WHERE
+      c.post_id = $1
+    ORDER BY
+      c.created_at
+    `,
+    [post_id]
+  );
+
+  return rows;
+}
+
 export const commentsRepositories = {
   postComment,
+  getComments,
 };
--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { postComment } from "../controllers/comments.controllers.js";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
+import { postIdValidation } from "../middlewares/posts/postIdValidation.middleware.js";
 import { validateBody } from "../middlewares/schema.middleware.js";
 import { commentSchema } from "../schemas/comments.schema.js";
 
 const commentsRouter = Router();
 
-commentsRouter.post("/comments", authValidation, validateBody(commentSchema, "comment"), postComment);
+commentsRouter.post("/comments/:post_id", authValidation, validateBody(commentSchema, "comment"), postIdValidation, postComment);
 
 export default commentsRouter;
